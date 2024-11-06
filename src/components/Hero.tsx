09b5c72859c3b0824327c86080998e434b553871
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-11-06 17:08:16 +0545

    update docs, idea, layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ const Hero = () => {
         <section className="relative bg-transparent py-20 md:py-32">
             <div className="container mx-auto px-6 lg:px-12 text-center md:max-w-4xl rounded-lg p-10">
                 {/* Main Headline */}
-                <h2 className="text-center text-3xl font-medium text-gray-900 sm:text-6xl">
+                <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-muted-foreground sm:text-7xl text-gray-900">
                     Real-Time Electric Vehicle Data and Resources
-                </h2>
+                </h1>
                 {/* text-center text-3xl font-medium text-gray-900 dark:text-gray-50
                 sm:text-6xl */}
                 {/* Subtitle */}
